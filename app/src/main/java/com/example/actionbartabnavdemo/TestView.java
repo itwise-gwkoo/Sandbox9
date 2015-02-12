b0ca823b75d4595226226a89c57c3dc4ac642a16
Dennis,Koo <kkuroo@gmail.com>
2015-02-12 11:28:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/actionbartabnavdemo/TestView.java
@@ -0,0 +1,19 @@
+package com.example.actionbartabnavdemo;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by kkuru on 2015-02-04.
+ */
+public class TestView extends View implements View.OnClickListener{
+	public TestView(Context context) {
+		super(context);
+	}
+
+	@Override
+	public void onClick(View v) {
+		Log.i("MyActivity", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxr ");
+	}
+}
